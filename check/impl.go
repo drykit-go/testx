af3054b4878559515a3560a11599e6184c620d81
Gregory Albouy <gregoryalbouy75@gmail.com>
2021-08-25 00:54:52 +0200

    Add doc comments to package check

--- check/impl.go
+++ check/impl.go
@@ -6,55 +6,105 @@ import (
 )
 
 type (
+	// BytesNativeChecks provides checks on type []byte.
 	BytesNativeChecks interface {
+		// Equal checks the gotten []byte is equal to the target.
 		Equal(tar []byte) BytesChecker
+		// EqualJSON checks the gotten []byte and the target returns
+		// the same JSON object.
 		EqualJSON(tar []byte) BytesChecker
+		// Len checks the gotten []byte's length passes the provided
+		// IntChecker.
 		Len(c IntChecker) BytesChecker
 	}
 
+	// StringNativeChecks provides checks on type string.
 	StringNativeChecks interface {
+		// Equal checks the gotten string is equal to the target.
 		Equal(tar string) StringChecker
+		// Contains checks the gotten string contains the target substring.
 		Contains(tar string) StringChecker
+		// NotContains checks the gotten string do not contain the target
+		// substring.
 		NotContains(tar string) StringChecker
+		// Match checks the gotten string matches the given regexp.
 		Match(rgx *regexp.Regexp) StringChecker
+		// NotMatch checks the gotten string do not match the given regexp.
 		NotMatch(rgx *regexp.Regexp) StringChecker
+		// Len checks the gotten string's length passes the given IntChecker.
 		Len(c IntChecker) StringChecker
 	}
 
+	// IntNativeChecks provides checks on type int.
 	IntNativeChecks interface {
+		// InRange checks the gotten int is in the closed interval [lo:hi].
 		InRange(lo, hi int) IntChecker
+		// NotInRange checks the gotten int is not in the closed interval [lo:hi].
 		NotInRange(lo, hi int) IntChecker
+		// Equal checks the gotten int is equal to the target.
 		Equal(tar int) IntChecker
+		// NotEqual checks the gotten int is not equal to the target.
 		NotEqual(tar int) IntChecker
+		// GreaterThan checks the gotten int is greater than the target.
 		GreaterThan(tar int) IntChecker
+		// GreaterOrEqual checks the gotten int is greater or equal to the target.
 		GreaterOrEqual(tar int) IntChecker
+		// LesserThan checks the gotten int is lesser than the target.
 		LesserThan(tar int) IntChecker
+		// LesserOrEqual checks the gotten int is lesser or equal to the target.
 		LesserOrEqual(tar int) IntChecker
 	}
 
+	// DurationNativeChecks provides checks on type time.Duration.
 	DurationNativeChecks interface {
+		// Over checks the gotten time.Duration is over the target duration.
 		Over(tar time.Duration) DurationChecker
+		// Under checks the gotten time.Duration is under the target duration.
 		Under(tar time.Duration) DurationChecker
 	}
 
+	// HTTPHeaderNativeChecks provides checks on type http.Header.
 	HTTPHeaderNativeChecks interface {
+		// KeySet checks the gotten http.Header has a spcific key set.
+		// The corresponding value is ignored, meaning an empty value
+		// for that key passes the check.
 		KeySet(key string) HTTPHeaderChecker
+		// KeyNotSet checks the gotten http.Header does not have
+		// a specific key set.
 		KeyNotSet(key string) HTTPHeaderChecker
+		// ValueSet checks the gotten http.Heaser has any key
+		// witha a matching value.
 		ValueSet(val string) HTTPHeaderChecker
+		// ValueNotSet checks the gotten http.Header does not have
+		// any key with a matching value.
 		ValueNotSet(val string) HTTPHeaderChecker
+		// ValueOf checks the gotten http.Header has a value
+		// for the matching key that passes the given StringChecker.
 		ValueOf(key string, c StringChecker) HTTPHeaderChecker
 	}
 
+	// UntypedNativeChecks provides checks on type interface{}.
 	UntypedNativeChecks interface {
+		// Custom checks the gotten value passes the given UntypedPassFunc.
+		// The description should typically begin with keywords like "expect"
+		// or "should" for intelligible output.
+		// For instance, "expect odd number" would output:
+		// 	> "expect odd number, got 42"
 		Custom(desc string, f UntypedPassFunc) UntypedChecker
 	}
 )
 
 var (
-	Bytes      BytesNativeChecks      = bytesCheckFactory{}      // Bytes provides checks on type []byte
-	String     StringNativeChecks     = stringCheckFactory{}     // String provides checks on type string
-	Int        IntNativeChecks        = intCheckFactory{}        // Int provides checks on type int
-	Duration   DurationNativeChecks   = durationCheckFactory{}   // Duration provides checks on type time.Duration
-	HTTPHeader HTTPHeaderNativeChecks = httpHeaderCheckFactory{} // HTTPHeader provides checks on type http.Header
-	Untyped    UntypedNativeChecks    = untypedCheckFactory{}    // Untyped provides checks on type interface{}
+	// Bytes implements BytesNativeChecks.
+	Bytes BytesNativeChecks = bytesCheckFactory{}
+	// String implements StringNativeChecks.
+	String StringNativeChecks = stringCheckFactory{}
+	// Int implements IntNativeChecks.
+	Int IntNativeChecks = intCheckFactory{}
+	// Duration implements DurationNativeChecks.
+	Duration DurationNativeChecks = durationCheckFactory{}
+	// HTTPHeader implements HTTPHeaderNativeChecks.
+	HTTPHeader HTTPHeaderNativeChecks = httpHeaderCheckFactory{}
+	// Untyped implements UntypedNativeChecks.
+	Untyped UntypedNativeChecks = untypedCheckFactory{}
 )
