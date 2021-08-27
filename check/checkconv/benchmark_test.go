74a39a5e5e92a1598f6e76aca331e6c5d06ae042
Gregory Albouy <gregoryalbouy75@gmail.com>
2021-08-27 23:01:19 +0200

    Fix and improve checkconv benchmarks

--- check/checkconv/benchmark_test.go
+++ check/checkconv/benchmark_test.go
@@ -3,14 +3,15 @@ package checkconv_test
 import (
 	"testing"
 
+	"github.com/drykit-go/testx/check"
 	"github.com/drykit-go/testx/check/checkconv"
 )
 
 func BenchmarkAll(b *testing.B) {
 	b.Run("IsChecker", BenchmarkIsChecker)
 	b.Run("Cast", BenchmarkCast)
-	b.Run("Assert", BenchmarkIsChecker)
-	b.Run("FromInt", BenchmarkFromInt)
+	b.Run("Assert", BenchmarkAssert)
+	b.Run("From", BenchmarkFrom)
 }
 
 func BenchmarkIsChecker(b *testing.B) {
@@ -20,6 +21,27 @@ func BenchmarkIsChecker(b *testing.B) {
 	}
 }
 
+func BenchmarkAssert(b *testing.B) {
+	b.Run("check.BytesChecker first_case", func(b *testing.B) {
+		c := check.Bytes.Is([]byte{0})
+		for i := 0; i < b.N; i++ {
+			checkconv.Assert(c)
+		}
+	})
+	b.Run("check.IntChecker midway_case", func(b *testing.B) {
+		c := check.Int.Is(0)
+		for i := 0; i < b.N; i++ {
+			checkconv.Assert(c)
+		}
+	})
+	b.Run("check.HTTPHeaderChecker last_case", func(b *testing.B) {
+		c := check.HTTPHeader.KeySet("")
+		for i := 0; i < b.N; i++ {
+			checkconv.Assert(c)
+		}
+	})
+}
+
 func BenchmarkCast(b *testing.B) {
 	c := validCheckerInt{}
 	for i := 0; i < b.N; i++ {
@@ -27,14 +49,7 @@ func BenchmarkCast(b *testing.B) {
 	}
 }
 
-func BenchmarkAssert(b *testing.B) {
-	c := validCheckerInt{}
-	for i := 0; i < b.N; i++ {
-		checkconv.IsChecker(c)
-	}
-}
-
-func BenchmarkFromInt(b *testing.B) {
+func BenchmarkFrom(b *testing.B) {
 	c := validCheckerInt{}
 	for i := 0; i < b.N; i++ {
 		checkconv.FromInt(c)
