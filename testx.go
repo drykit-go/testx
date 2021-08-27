9ad84222177b78b850b8c278621fdaa97440d000
Gregory Albouy <gregoryalbouy75@gmail.com>
2021-08-27 23:51:31 +0200

    Improve testx.go readability
    - Remove 1 indent level due to chained type declarations
    - Add comments titles to separate interfaces kinds

--- testx.go
+++ testx.go
@@ -8,58 +8,63 @@ import (
 	"github.com/drykit-go/testx/check"
 )
 
-type (
+//
+// Runners interfaces
+//
 
-	// Runner provides a method Run to perform various tests.
-	Runner interface {
-		// Run runs a test and fails it if a check does not pass.
-		Run(t *testing.T)
-	}
+// Runner provides a method Run to perform various tests.
+type Runner interface {
+	// Run runs a test and fails it if a check does not pass.
+	Run(t *testing.T)
+}
 
-	// ValueRunner provides methods to perform tests on a single value.
-	ValueRunner interface {
-		Runner
-		// DryRun returns a Resulter to access test results
-		// without running it.
-		DryRun() Resulter
-		// Exp adds an equality check on the tested value.
-		Exp(value interface{}) ValueRunner
-		// ExpNot adds inequality checks on the tested value.
-		ExpNot(values ...interface{}) ValueRunner
-		// Pass adds checkers on the tested value.
-		Pass(checkers ...interface{}) ValueRunner
-	}
+// ValueRunner provides methods to perform tests on a single value.
+type ValueRunner interface {
+	Runner
+	// DryRun returns a Resulter to access test results
+	// without running it.
+	DryRun() Resulter
+	// Exp adds an equality check on the tested value.
+	Exp(value interface{}) ValueRunner
+	// ExpNot adds inequality checks on the tested value.
+	ExpNot(values ...interface{}) ValueRunner
+	// Pass adds checkers on the tested value.
+	Pass(checkers ...interface{}) ValueRunner
+}
 
-	// TableRunner provides methods to perform tests on a given func
-	// using a slice of Case.
-	TableRunner interface {
-		Runner
-		// DryRun returns a TableResulter to access test results
-		// without running it.
-		DryRun() TableResulter
-		// Cases adds test cases to be run on the tested func.
-		Cases(cases []Case) TableRunner
-	}
+// TableRunner provides methods to perform tests on a given func
+// using a slice of Case.
+type TableRunner interface {
+	Runner
+	// DryRun returns a TableResulter to access test results
+	// without running it.
+	DryRun() TableResulter
+	// Cases adds test cases to be run on the tested func.
+	Cases(cases []Case) TableRunner
+}
 
-	// HandlerRunner provides methods to perform tests on a http.Handler
-	// or http.HandlerFunc.
-	HandlerRunner interface {
-		Runner
-		// DryRun returns a HandlerResulter to access test results
-		// without running it.
-		DryRun() HandlerResulter
-		// ResponseHeader adds checkers on the response header.
-		ResponseHeader(...check.HTTPHeaderChecker) HandlerRunner
-		// ResponseHeader adds checkers on the response status.
-		ResponseStatus(...check.StringChecker) HandlerRunner
-		// ResponseHeader adds checkers on the response code.
-		ResponseCode(...check.IntChecker) HandlerRunner
-		// ResponseHeader adds checkers on the response body.
-		ResponseBody(...check.BytesChecker) HandlerRunner
-		// ResponseHeader adds checkers on the handling duration.
-		Duration(...check.DurationChecker) HandlerRunner
-	}
-)
+// HandlerRunner provides methods to perform tests on a http.Handler
+// or http.HandlerFunc.
+type HandlerRunner interface {
+	Runner
+	// DryRun returns a HandlerResulter to access test results
+	// without running it.
+	DryRun() HandlerResulter
+	// ResponseHeader adds checkers on the response header.
+	ResponseHeader(...check.HTTPHeaderChecker) HandlerRunner
+	// ResponseHeader adds checkers on the response status.
+	ResponseStatus(...check.StringChecker) HandlerRunner
+	// ResponseHeader adds checkers on the response code.
+	ResponseCode(...check.IntChecker) HandlerRunner
+	// ResponseHeader adds checkers on the response body.
+	ResponseBody(...check.BytesChecker) HandlerRunner
+	// ResponseHeader adds checkers on the handling duration.
+	Duration(...check.DurationChecker) HandlerRunner
+}
+
+//
+// Results interfaces
+//
 
 // Resulter provides methods to read test results after a dry run.
 type Resulter interface {
@@ -97,6 +102,8 @@ type HandlerResulter interface {
 	ResponseDuration() time.Duration
 }
 
+// TableResulter provides methods to read TableRunner results
+// after a dry run.
 type TableResulter interface {
 	Resulter
 	// PassedAt returns true if the ith test case passed.
