d5ac9cbe0a75596a7b5481bcc6b7ad45acdb0318
Gregory Albouy <gregoryalbouy75@gmail.com>
2021-09-09 14:06:23 +0200

    Move ComputeDocLines to dedicated file

--- internal/gen/astserializer/serializer.go
+++ internal/gen/astserializer/serializer.go
@@ -10,31 +10,6 @@ import (
 // to write serialized representation of go/ast types.
 type stringBuilder struct{ strings.Builder }
 
-// ComputeDocLines parses the raw doc (as returned by go/doc.Type.Doc),
-// applies the given replacements and returns a slice of strings representing
-// the resulting lines.
-func ComputeDocLines(rawdoc string, repl map[string]string) []string {
-	lines := []string{}
-	if rawdoc == "" {
-		return lines
-	}
-
-	// Set up strings replacer
-	replArgs := []string{}
-	for k, v := range repl {
-		replArgs = append(replArgs, k, v)
-	}
-	replacer := strings.NewReplacer(replArgs...)
-
-	// Parse and add new lines
-	for _, l := range strings.Split(rawdoc, "\n") {
-		lines = append(lines, replacer.Replace(l))
-	}
-
-	// Strip last line, always empty due to final '\n'
-	return lines[:len(lines)-1]
-}
-
 // BuildFuncSignature builds a func signature given a name an *ast.FuncType
 // and returns it as a string.
 func BuildFuncSignature(name string, ftyp *ast.FuncType) string {
