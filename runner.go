7749b1b70ee8cf440cd1e0c0a848dbf8a953cbbe
Gregory Albouy <gregoryalbouy75@gmail.com>
2021-10-17 21:27:01 +0200

    Remove baseRunner unused methods

--- runner.go
+++ runner.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/drykit-go/testx/check"
-	"github.com/drykit-go/testx/checkconv"
 )
 
 type (
@@ -23,8 +22,8 @@ type baseRunner struct {
 	checks []baseCheck
 }
 
-func (r *baseRunner) addCheck(c baseCheck) {
-	r.checks = append(r.checks, c)
+func (r *baseRunner) addCheck(bc baseCheck) {
+	r.checks = append(r.checks, bc)
 }
 
 func (r *baseRunner) addChecks(label string, get getfunc, checkers []check.ValueChecker, safe bool) {
@@ -32,42 +31,6 @@ func (r *baseRunner) addChecks(label string, get getfunc, checkers []check.Value
 		if !safe && !checkconv.IsChecker(c) {
 			panic("invalid checker provided to MustPass")
 		}
-		r.addCheck(baseCheck{label: label, get: get, checker: c})
-	}
-}
-
-func (r *baseRunner) addIntChecks(label string, get getfunc, checks []check.IntChecker) {
-	for _, c := range checks {
-		r.addCheck(baseCheck{label: label, get: get, checker: checkconv.FromInt(c)})
-	}
-}
-
-func (r *baseRunner) addBytesChecks(label string, get getfunc, checks []check.BytesChecker) {
-	for _, c := range checks {
-		r.addCheck(baseCheck{label: label, get: get, checker: checkconv.FromBytes(c)})
-	}
-}
-
-func (r *baseRunner) addStringChecks(label string, get getfunc, checks []check.StringChecker) {
-	for _, c := range checks {
-		r.addCheck(baseCheck{label: label, get: get, checker: checkconv.FromString(c)})
-	}
-}
-
-func (r *baseRunner) addDurationChecks(label string, get getfunc, checks []check.DurationChecker) {
-	for _, c := range checks {
-		r.addCheck(baseCheck{label: label, get: get, checker: checkconv.FromDuration(c)})
-	}
-}
-
-func (r *baseRunner) addHTTPHeaderChecks(label string, get getfunc, checks []check.HTTPHeaderChecker) {
-	for _, c := range checks {
-		r.addCheck(baseCheck{label: label, get: get, checker: checkconv.FromHTTPHeader(c)})
-	}
-}
-
-func (r *baseRunner) addUntypedChecks(label string, get getfunc, checks []check.ValueChecker) {
-	for _, c := range checks {
 		r.addCheck(baseCheck{label: label, get: get, checker: c})
 	}
 }
