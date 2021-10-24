c1a9cea1abd964f163cd521c4a667289d0174f32
Gregory Albouy <gregoryalbouy75@gmail.com>
2021-10-24 16:07:33 +0200

    Add doc comment to `testutil.AssertPanic`

--- internal/testutil/testutil.go
+++ internal/testutil/testutil.go
@@ -2,6 +2,13 @@ package testutil
 
 import "testing"
 
+// AssertPanic fails t if the calling func does not panic
+// with the expected message. It must be called with defer:
+//
+// 	func TestFuncThatPanics(t *testing.T) {
+// 		defer testutil.AssertPanic(t, "oops, I panicked")
+// 		FuncThatPanics()
+// 	}
 func AssertPanic(t *testing.T, expMessage string) {
 	t.Helper()
 	r := recover()
