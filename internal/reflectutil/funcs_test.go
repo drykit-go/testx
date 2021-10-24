942aab19d5a6b303b63b4d12a14ad7516fee7d58
Gregory Albouy <gregoryalbouy75@gmail.com>
2021-10-24 20:27:13 +0200

    Write unit test for NewFunc

--- internal/reflectutil/funcs_test.go
+++ internal/reflectutil/funcs_test.go
@@ -1,12 +1,16 @@
 package reflectutil_test
 
 import (
-	"net/http"
+	"errors"
 	"testing"
 
 	"github.com/drykit-go/testx/internal/reflectutil"
 	"github.com/drykit-go/testx/internal/testutil"
 )
+
+func validFunc(in int) int {
+	return in
+}
 
 func TestFuncName(t *testing.T) {
 	t.Run("non-func panics", func(t *testing.T) {
@@ -20,8 +24,36 @@ func TestFuncName(t *testing.T) {
 	})
 
 	t.Run("returns func name", func(t *testing.T) {
-		if got, exp := reflectutil.FuncName(http.Get), "http.Get"; got != exp {
+		got := reflectutil.FuncName(validFunc)
+		exp := "reflectutil_test.validFunc"
+		if got != exp {
 			t.Errorf("exp %s, got %s", exp, got)
+		}
+	})
+}
+
+func TestNewFunc(t *testing.T) {
+	t.Run("non-func returns error", func(t *testing.T) {
+		nofunc := struct{}{}
+		f, err := reflectutil.NewFunc(nofunc)
+		if err == nil {
+			t.Error("exp non-nil error, got nil")
+		}
+		if !errors.Is(err, reflectutil.ErrNotAFunc) {
+			t.Errorf("got unexpected error: %s", err)
+		}
+		if f != nil {
+			t.Errorf("exp nil *Func, got %v", f)
+		}
+	})
+
+	t.Run("valid func returns *Func", func(t *testing.T) {
+		f, err := reflectutil.NewFunc(validFunc)
+		if err != nil {
+			t.Errorf("got unexpected error: %s", err)
+		}
+		if got, exp := f.Name, reflectutil.FuncName(validFunc); got != exp {
+			t.Errorf("f.Name: exp %s, got %s", exp, got)
 		}
 	})
 }
