21bea13789d4e2b8a27ace41e35c5d069eabf1ac
Gregory Albouy <gregoryalbouy75@gmail.com>
2021-10-25 18:56:59 +0200

    Refactor check utils

--- check/util.go
+++ check/util.go
@@ -2,41 +2,42 @@ package check
 
 import (
 	"encoding/json"
-	"log"
+	"fmt"
 	"reflect"
 )
 
 // sameJSONproduced returns true if xdata and ydata result in the same JSON value,
 // ignoring the keys order.
-// xptr and yptr must be pointers to interface{}, as their values are filled
+// xptr and yptr must be pointers, as their values are filled
 // with marshaled+unmarshaled json from xdata and ydata respectively.
 //
 // It panics on the first error encountered in the process.
 func sameJSONproduced(xdata, ydata, xptr, yptr interface{}) bool {
-	bx, err := json.Marshal(xdata)
-	if err != nil {
-		log.Panicf("failed to marshal value: %#v\n%v", xdata, err)
+	mustMarshal := func(in interface{}) []byte {
+		b, err := json.Marshal(in)
+		if err != nil {
+			panic(fmt.Sprintf("failed to marshal value: %#v:\n%v", b, err))
+		}
+		return b
 	}
-	by, err := json.Marshal(ydata)
-	if err != nil {
-		log.Panicf("failed to marshal value: %#v\n%v", ydata, err)
-	}
+	bx, by := mustMarshal(xdata), mustMarshal(ydata)
 	return sameJSON(bx, by, &xptr, &yptr)
 }
 
 // sameJSON returns true if x and y evaluate to the same JSON value,
 // ignoring the keys order.
-// xptr and yptr must be pointers to interface{}, as their values are filled
+// xptr and yptr must be pointers, as their values are filled
 // with unmarshaled x and y respectively.
 //
 // It panics on the first error encountered in the process.
 func sameJSON(x, y []byte, xptr, yptr interface{}) bool {
-	if err := json.Unmarshal(x, &xptr); err != nil {
-		log.Panic(err)
+	mustUnmarshal := func(b []byte, ptr interface{}) {
+		if err := json.Unmarshal(b, &ptr); err != nil {
+			panic(err)
+		}
 	}
-	if err := json.Unmarshal(y, &yptr); err != nil {
-		log.Panic(err)
-	}
+	mustUnmarshal(x, xptr)
+	mustUnmarshal(y, yptr)
 	return reflect.DeepEqual(xptr, yptr)
 }
 
